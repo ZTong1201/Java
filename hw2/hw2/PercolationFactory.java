4050d9caf84e63ba2db4c22b5509d948a2d9b70c
ZTong1201 <buaatzy@163.com>
2019-05-18 14:27:51 -0400

    Remove duplicate folders

--- hw2/hw2/PercolationFactory.java
+++
@@ -1,7 +0,0 @@
-package hw2;
-
-public class PercolationFactory {
-    public Percolation make(int N) {
-        return new Percolation(N);
-    }
-}
--- hw3/hw3/hash/Oomage.java
+++
@@ -1,5 +0,0 @@
-package hw3.hash;
-
-public interface Oomage {
-    void draw(double x, double y, double scalingFactor);
-} 
